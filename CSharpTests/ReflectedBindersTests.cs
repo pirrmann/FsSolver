8eb2e2217a7fe6907cddc29d43b9ae3474b35b6a
Pierre IRRMANN <pierre.irrmann@gmail.com>
2014-12-23 11:25:11 +0100

    Error reporting test

--- CSharpTests/ReflectedBindersTests.cs
+++ CSharpTests/ReflectedBindersTests.cs
@@ -63,5 +63,32 @@ namespace ReflectedBindersTests
             Assert.AreEqual(200M, underlying.ExecFees);
             Assert.AreEqual(0.1M, underlying.WeightedDelta);
         }
+
+        [Test]
+        public void Error_reporting_from_C_Sharp_with_reflected_binders()
+        {
+            var rfq = new Rfq
+            {
+                FeesPerLot = 1.0M,
+                Underlyings = new[] {
+                    new Underlying {
+                        BaseSize = 100M,
+                        WeightedDelta = 0.11M,
+                        Legs = new [] {
+                            new Leg { Size = 100M, Delta = 0.4M },
+                            new Leg { Size = 100M, Delta = -0.3M }
+                        }
+                    }
+                }
+            };
+
+            var settings = new CSharpTestsRules.Settings(true);
+            var rules = CSharpTestsRules.GetRules(settings);
+
+            var problem = BoundProblem.Create(rules, rfq);
+            var incoherencies = problem.Solve();
+
+            Assert.AreEqual(incoherencies.Length, 6);
+        }
     }
 }
